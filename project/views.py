db9281a4ecb8c5085c0b88a1c239448af64f0cf8
SiddhantGhimire <siddhantghimire7@gmail.com>
2025-07-18 15:08:11 +0545

    initial commit

---
+++ project/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
+
+
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            
+            messages.success(request, 'Message sent successfully!')
+            return redirect('#contact')  # reloads home page with anchor
+    else:
+        form = ContactForm()
+
+    return render(request, 'home.html', {'form': form})
+
